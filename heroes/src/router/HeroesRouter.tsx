20ed7634ce890e4798876d9dd26b5275e3878ba1
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 13:27:41 -0600

    [FEAT] Cambios en el componente HeroesRouter
    - importando el componente BrowserRouter , BrowserRouter no servira para poder usar react router dom en el navegador
    - envolviendo el componente BrowserRouter en la devolución del componente HeroesRouter

--- heroes/src/router/HeroesRouter.tsx
+++ heroes/src/router/HeroesRouter.tsx
@@ -1,7 +1,9 @@
-
+import { BrowserRouter } from 'react-router-dom'
 
 export const HeroesRouter = () => {
   return (
-    <div>HeroesRouter</div>
+    <BrowserRouter>
+    
+    </BrowserRouter>
   )
 }
