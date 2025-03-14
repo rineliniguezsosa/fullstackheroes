d29d022c91cc1e4f0b998b9985ac06bc6abf8f0f
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 14:37:22 -0600

    [FEAT] Multiples cambios en el archivo Nav
    - importe los iconos Brightness5Icon y ModeNightIcon
    - cambie los atributos css en el nav, estableci un borde de color rojo, cambie el padding a 5rem, display flex para que mis iconos estén justificados entre el contenedor
    - estableci en elemento div el cual contiene mi iconos

--- heroes/src/components/layout/nav/Nav.tsx
+++ heroes/src/components/layout/nav/Nav.tsx
@@ -1,10 +1,15 @@
 import { NavLink } from "react-router-dom"
-
+import Brightness5Icon from '@mui/icons-material/Brightness5';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 export const Nav = () => {
   return (
-    <nav className="w-full px-10 py-10">
+    <nav className="w-full border-2 border-red-500 p-5 flex justify-between">
         <NavLink to="/">Home</NavLink>
+
+        <div>
+          <Brightness5Icon/> / <ModeNightIcon/>
+        </div>
     </nav>
   )
 }
