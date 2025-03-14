9bb3bc6760fa67e8d4a0d083430e7f7f3ac6d1a5
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 13:42:33 -0600

    [FEAT] Importando el componente Nav en mi componente HeroesRouter para mostrar el nav de nuestra aplicación para que pueda cambiar dinamicamente de vistas al editar un heroe ó los elementos de marvel

--- heroes/src/router/HeroesRouter.tsx
+++ heroes/src/router/HeroesRouter.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { Home } from '../components/feature/heroes'
+import { Nav } from '../components/layout/nav/Nav'
 
 export const HeroesRouter = () => {
   return (
     <BrowserRouter>
+        <Nav></Nav>
         <Routes>
             <Route path='/' Component={Home}></Route>
         </Routes>
