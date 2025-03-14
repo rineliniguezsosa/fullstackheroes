b9cf484d7e814d71f1334e0e535a3c72633d0b93
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 15:04:18 -0600

    [FEAT] Cambios en el archivo principal del servidor
    - establecemos las configuraciones del servidor para que solo lea las entradas en formato json
    - estableciendo el puerto para escuchar los cambios y estableciendo un mensaje para observar la ejecución del servidor

--- heroesapi/src/index.ts
+++ heroesapi/src/index.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
+app.use(express.json())
 
-
-app.listen(()=>{
-    console.log();
+app.listen(`${process.env.PORT}`,()=>{
+    console.log(`El servidor esta corriendo en http://localhost:${process.env.PORT}`);
 })
