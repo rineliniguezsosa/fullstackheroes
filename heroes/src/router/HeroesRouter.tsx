aa05e74c37d40e157ed0a9f25e27436f3c11fec4
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 15:26:10 -0600

    [FEAT] Actualizando la url para acceder a nuestro componente Home

--- heroes/src/router/HeroesRouter.tsx
+++ heroes/src/router/HeroesRouter.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import { Home } from '../components/feature/heroes'
+import { Home } from '../components'
 import { Nav } from '../components/layout/nav/Nav'
 
 export const HeroesRouter = () => {
